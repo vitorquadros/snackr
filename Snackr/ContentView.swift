93c048ea158873231478e6811a353ea3418af605
Vitor Quadros <viitorquadros@hotmail.com>
2025-10-14 21:04:28 -0300

    Initial Commit

---
+++ Snackr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Snackr
+//
+//  Created by Vitor Quadros on 14/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Snackr/SnackrApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnackrApp.swift
+//  Snackr
+//
+//  Created by Vitor Quadros on 14/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SnackrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
