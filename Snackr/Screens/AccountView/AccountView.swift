92861df4f4826471617c58cccae1141ab80af03a
Vitor Quadros <viitorquadros@hotmail.com>
2025-10-30 23:10:51 -0300

    feat: improve keyboard usability on forms

--- Snackr/Screens/AccountView/AccountView.swift
+++ Snackr/Screens/AccountView/AccountView.swift
@@ -9,14 +9,30 @@ import SwiftUI
 
 struct AccountView: View {
     @StateObject var vm = AccountViewModel()
+    @FocusState private var focusedTextField: FormTextField?
+    
+    enum FormTextField {
+        case firstName, lastName, email
+    }
     
     var body: some View {
         NavigationStack {
             Form {
                 Section("Personal Info") {
                     TextField("First Name", text: $vm.user.firstName)
+                        .focused($focusedTextField, equals: .firstName)
+                        .onSubmit { focusedTextField = .lastName }
+                        .submitLabel(.next)
+                    
                     TextField("Last Name", text: $vm.user.lastName)
+                        .focused($focusedTextField, equals: .lastName)
+                        .onSubmit { focusedTextField = .email }
+                        .submitLabel(.next)
+                    
                     TextField("Email", text: $vm.user.email)
+                        .focused($focusedTextField, equals: .email)
+                        .onSubmit { focusedTextField = nil }
+                        .submitLabel(.continue)
                         .keyboardType(.emailAddress)
                         .autocapitalization(.none)
                         .autocorrectionDisabled()
@@ -38,6 +54,11 @@ struct AccountView: View {
                 .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
             }
             .navigationTitle("Account")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Button("Dismiss") { focusedTextField = nil }
+                }
+            }
         }
         .onAppear {
             vm.retrieveUser()
